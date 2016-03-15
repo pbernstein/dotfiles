889ab5bb7a30f19de0bab130b4cc7c52646997b0
Peter Bernstein <Peter Bernstein>
2016-03-14 21:51:57 -0500

    fixed linefeeds

--- core/link.sh
+++ core/link.sh
@@ -2,6 +2,6 @@ mv ~/.vim ~/.vim_old
 mv ~/.vimrc ~/.vimrc_old
 mv ~/.bashrc ~/.bashrc_old
 
-ln -s /home/`whoami`/git/dotfiles/core/vim/ ~/.vim
-ln -s /home/`whoami`/git/dotfiles/core/vim/.vimrc ~/.vimrc
-ln -s /home/`whoami`/git/dotfiles/core/bash/bashrc ~/.bashrc
+ln -s /home/$(whoami)/git/dotfiles/core/vim/ ~/.vim
+ln -s /home/$(whoami)/git/dotfiles/core/vim/.vimrc ~/.vimrc
+ln -s /home/$(whoami)/git/dotfiles/core/bash/bashrc ~/.bashrc
